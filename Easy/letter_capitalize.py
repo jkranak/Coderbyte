0a34c70300815fc309ce480b520a42ece47bbf83
Joseph Kranak <jakranak@gmail.com>
2022-12-13 15:50:41 -0600

    Create letter_capitalize.py

---
+++ Easy/letter_capitalize.py
@@ -0,0 +1,4 @@
+# Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space.
+
+def LetterCapitalize(str): 
+  return str.title()
