29dfbb7f964b6d255e6f15330147ae8e67800ba4
Joseph Kranak <jakranak@gmail.com>
2022-12-19 12:53:28 -0600

    Create word_count.py

---
+++ Easy/word_count.py
@@ -0,0 +1,4 @@
+# Have the function WordCount(str) take the str string parameter being passed and return the number of words the string contains. Words will be separated by single spaces.
+
+def WordCount(strParam):
+  return len(strParam.split())
