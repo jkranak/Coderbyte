a649bbc7f1d8606e9d1e395f599f4f053cb546cd
Joseph Kranak <jakranak@gmail.com>
2022-12-20 13:46:58 -0600

    Update multiple_brackets.py

--- Medium/multiple_brackets.py
+++ Medium/multiple_brackets.py
@@ -10,16 +10,16 @@ def MultipleBrackets(strParam):
   # equal the number of close parentheses and brackets, this is invalid
   if all_brackets.count('(') != all_brackets.count(')') or all_brackets.count('[') != all_brackets.count(']'):
     return 0
-  # Our strategy is to continually remove () and []. If we can't ever do it, it's invalid.
-  # We can't use the straegy we used in bracket_matcher.py(https://github.com/jkranak/Coderbyte/blob/main/Medium/bracket_matcher.py)
-  # because in fails in cases like '[(])', that is in cases of a pair of brackets and parentheses overlapping
-  while len(all_brackets) > 0:
-    if '()' in all_brackets:
-      all_brackets = all_brackets.replace('()', '')
+  # open_stack is a stack of open brakets and parentheses. Our strategy it to check that close brackets match the last bracket on the stack
+  open_stack = []
+  for b in all_brackets:
+    if b == '(' or b == '[':
+      open_stack.append(b)
       continue
-    if '[]' in all_brackets:
-      all_brackets = all_brackets.replace('[]', '')
-      continue
-    return 0
+    if len(open_stack) == 0:
+      return 0
+    last_open = open_stack.pop(-1)
+    if (last_open == '(') == (b == ']'):
+      return 0
   
   return f'1 {int(length / 2)}'
